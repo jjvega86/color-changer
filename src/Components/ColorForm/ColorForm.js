1bd6870811cd290e0431308512b37b398d1c8a03
JJ <jj.vega86@gmail.com>
2021-03-09 11:02:51 -0500

    Added colorOptions and map function to render options to form element in ColorForm

--- src/Components/ColorForm/ColorForm.js
+++ src/Components/ColorForm/ColorForm.js
@@ -1,12 +1,40 @@
 import React from "react";
 
+const colorOptions = [
+    {
+        value: "red",
+        text: "Red"
+    },
+    {
+        value: "blue",
+        text: "Blue"
+    },
+    {
+        value: "yellow",
+        text: "Yellow"
+    },
+    {
+        value: "green",
+        text: "Green"
+    },
+    {
+        value: "purple",
+        text: "Purple"
+    },
+]
+
 const ColorForm = (props) => {
+
+    const renderedOptions = colorOptions.map(({value, text}) => {
+        return <option value={value}>{text}</option>
+    })
   return (
     <div className="ui form">
       <div className="field">
         <label>Pick a color!</label>
         <select className="ui search dropdown">
           <option value="">Select Color</option>
+          {renderedOptions}
         </select>
       </div>
     </div>
